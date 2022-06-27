c195e91c1e474223261873c468e3b27910212b11
Viktor <vitalineage22@gmail.com>
2022-06-27 19:48:09 +0300

    feat: Task01 completed

--- CSharpCourse/Less04.Denisov/Program.cs
+++ CSharpCourse/Less04.Denisov/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hey there!");
+DateTime timenow = DateTime.Now;
+Console.WriteLine($"Now is {timenow:T}");
